ea036dd65b3586efcbd8606fa76157513e179b57
minami <minaminaoy@gmail.com>
2020-04-12 18:52:28 +0900

    Add atcoder submissions

---
+++ install_snippets.py
@@ -0,0 +1,45 @@
+import glob
+import os
+import json
+
+SNIPPETS_DIR = "./snippets"
+
+FILES = glob.glob(os.path.join(SNIPPETS_DIR, "*"))
+print(f"files: {FILES}")
+snippets_dict = dict()
+
+SCOPE = 'cpp'
+
+for filepath in FILES:
+    print(filepath)
+    filename = filepath.split('/')[-1]
+    print(filename)
+
+    prefix = filename.split('.')[0]
+    with open(filepath, 'r') as snippet:
+        body = snippet.read()
+
+    snippets_dict[filename] = dict()
+    snippets_dict[filename]['prefix'] = prefix
+    snippets_dict[filename]['scope'] = SCOPE
+    snippets_dict[filename]['body'] = body
+    # print(snippets_dict)
+
+
+"""
+snippet example
+
+"Print to console": {
+	"scope": "javascript,typescript",
+	"prefix": "log",
+	"body": [
+		"console.log('$1');",
+		"$2"
+	],
+	"description": "Log output to console"
+}
+"""
+
+with open('./.vscode/snippet.code-snippets', 'w') as snippet_file:
+    json_str = json.dumps(snippets_dict)
+    snippet_file.write(json_str)
---
+++ snippets/tips.rs
@@ -0,0 +1,70 @@
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet};
+#[allow(unused_imports)]
+use std::io::*;
+#[allow(unused_imports)]
+use std::str::*;
+
+fn main() {
+    let a = 0;
+    let n = 0;
+
+    // HashSet
+    let mut st = HashSet::new();
+    st.insert(1);
+    st.contains(&a);
+
+    let n = format!("{:b}", n);
+
+    // Vec
+    let mut vec = Vec::new();
+    vec = Vec::with_capacity(100);
+
+    // String
+    let s: String;
+    s.chars().collect::<Vec<char>>();
+    s.chars().filter(|c| *c == '1').count();
+    s.chars().filter(|&c| c == 'o').count();
+    s.replace("2017", "2018");
+    v.iter().collect::<String>()
+
+    // 入力
+    input! {s:String}
+    let s = s.chars().collect::<Vec<char>>();
+
+    let mut ans = None;
+    for c in b'a'..b'z' + 1 {
+        let c = char::from(c);
+        ans = Some(c.to_string());
+    }
+    println!("{}", ans.unwrap_or("None".to_string()));
+
+    let ans: String = t.into_iter().collect();
+    println!(
+        "{}",
+        if a == b.chars().rev().collect::<String>() {
+            "YES"
+        } else {
+            "NO"
+        }
+    )
+
+    input! {
+        n: usize,
+        v: [(usize, usize); n],
+    }
+
+    for (a, b) in v {
+        
+    }
+
+    // 出力 定数倍高速化
+    let mut ans = String::new();
+    for i in 0..n {
+        ans.push_str(s);
+        ans.push('\n');
+    }
+    print!("{}", ans);
+}
